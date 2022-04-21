3da7fdc7e367cce81f2a3d2c16356d139789d7b1
Selina Shi <sshi26@live.unc.edu>
2022-04-21 19:50:52 -0400

    Prints id

--- server.js
+++ server.js
@@ -4,49 +4,44 @@ const fs = require("fs");
 const morgan = require("morgan");
 const app = express()
 
-const {db} = require('./database.js');
+// Require minimist module
+const args = require('minimist')(process.argv.slice(2))
+// See what is stored in the object produced by minimist
+console.log(args)
+// Store help text 
+const help = (`
+server.js [options]
 
-const myArgs = process.argv.slice(2);
-let portNumber = 0; 
-let isDebug = false; 
-let isLog = true; 
-for (let i = 0; i<myArgs.length; i++) {
-    const currentArgArray = myArgs[i].split('=')
-    const option = currentArgArray[0]; 
-    const value = currentArgArray[1]; 
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
 
-    switch (option) {
-        case "--port": 
-            portNumber = parseInt(value); 
-            break; 
-        case "--debug":
-            isDebug = value == "true" ? true:false; 
-            break;
-        case "--log": 
-            isLog = value == "true" ? true:false; 
-            break;
-        case "--help":
-            console.log(`server.js [options]
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
 
-            --port	Set the port number for the server to listen on. Must be an integer
-                        between 1 and 65535.
-          
-            --debug	If set to \`true\`, creates endlpoints /app/log/access/ which returns
-                        a JSON access log from the database and /app/error which throws 
-                        an error with the message "Error test successful." Defaults to 
-                        \`false\`.
-          
-            --log		If set to false, no log files are written. Defaults to true.
-                        Logs are always written to database.
-          
-            --help	Return this message and exit.`); 
-            return; 
-    }
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
 }
 
+const {db} = require('./database.js');
+
+
+let portNumber = args.port
+let isDebug = args.debug
+let isLog = args.log 
+
+
+
 app.use( (req, res, next) => {
-    const data = db.prepare("SELECT * FROM accesslog").get(); 
-    res.locals.data = data; 
+    
     let logdata = {
         remoteaddr: req.ip,
         remoteuser: req.user,
@@ -59,6 +54,14 @@ app.use( (req, res, next) => {
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
     }
+    const sqlStatement = `INSERT INTO accesslog (remoteaddr)
+        values (1234)`; 
+    db.prepare(sqlStatement).run(); 
+
+    const data = db.prepare("SELECT * FROM accesslog").get(); 
+    res.locals.data = data; 
+    console.log(data); 
+
     next();
 })
 
